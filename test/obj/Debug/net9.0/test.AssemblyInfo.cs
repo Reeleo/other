dca7f839d6aba46984d14dc7e3e2f4ed452c8c10
Reeleo <168227248+Reeleo@users.noreply.github.com>
2025-11-19 09:46:11 +0000

    det

--- test/obj/Debug/net9.0/test.AssemblyInfo.cs
+++ test/obj/Debug/net9.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0d6bef2130d970f46941995fde77fd5b75f40cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68f72ca6a85d744b8c5d6ec0e2472dd9e0839782")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
